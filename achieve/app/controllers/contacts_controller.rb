340b904364f881744f3bf9ca1bdc90b3326f073e
Kazu Murata <financeanyways@gmail.com>
2017-02-13 11:31:21 +0000

    dive04 bootstrap finish

---
+++ achieve/app/controllers/contacts_controller.rb
@@ -0,0 +1,24 @@
+class ContactsController < ApplicationController
+  def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact = Contact.new
+    end
+  end
+
+  def create
+    @contact = Contact.new(contacts_params)
+    redirect_to new_contact_path, notice: "お問い合わせが完了しました。"
+  end
+  
+  def confirm
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
